2330b28bdfcb64f65d8d6fd6e0d7fee8e61a9eb8
ashenfern <ashenfern@gmail.com>
2016-03-03 14:45:53 +0530

    checking changes + Adding widget Functionalities

---
+++ Mcd.RRMPortal/Mcd.RRMPortal.Business/WidgetManager.cs
@@ -0,0 +1,22 @@
+﻿using Mcd.RRMPortal.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcd.RRMPortal.Business
+{
+    public class WidgetManager
+    {
+        public void GetStoreProfiles()
+        {
+            var listStoreProfiles = new List<StoreProfile>() {
+            new StoreProfile(){DataValue ="asdfsdfaf ",Description = "asdfdsaf", LastChangedOn = "dfasdfds", LastDataCollection = "asdfsdafsf"},
+            new StoreProfile(){DataValue ="asdfsdfaf ",Description = "asdfdsaf", LastChangedOn = "dfasdfds", LastDataCollection = "asdfsdafsf"},
+            new StoreProfile(){DataValue ="asdfsdfaf ",Description = "asdfdsaf", LastChangedOn = "dfasdfds", LastDataCollection = "asdfsdafsf"},
+            new StoreProfile(){DataValue ="asdfsdfaf ",Description = "asdfdsaf", LastChangedOn = "dfasdfds", LastDataCollection = "asdfsdafsf"},
+            new StoreProfile(){DataValue ="asdfsdfaf ",Description = "asdfdsaf", LastChangedOn = "dfasdfds", LastDataCollection = "asdfsdafsf"}
+        };
+    }
+}
---
+++ Mcd.RRMPortal/Mcd.RRMPortal.Data/StoreProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcd.RRMPortal.Data
+{
+    public class StoreProfile
+    {
+        public String Description { get; set; }
+        public String DataValue { get; set; }
+        public String LastChangedOn { get; set; }
+        public String LastDataCollection { get; set; }
+    }
+}
