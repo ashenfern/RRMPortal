a9a8ceba498e7a3b0006c038db50405a94da5a8c
ashenfern <ashenfern@gmail.com>
2016-03-04 17:19:14 +0530

    checking changes + Widget changes

--- Mcd.RRMPortal/Mcd.RRMPortal.Web/Scripts/WidgetScripts/WidgetController.js
+++ Mcd.RRMPortal/Mcd.RRMPortal.Web/Scripts/WidgetScripts/WidgetController.js
@@ -1,14 +1,14 @@
 ﻿app.controller("widgetCtrl", function ($scope, widgetService) {
     $scope.divBook = false;
     GetAllStoreProfiles();
-    //To Get all book records  
+    //To Get all store profile records  
     function GetAllStoreProfiles() {
         debugger;
         var getStoreProfileData = widgetService.getStoreProfiles();
         getStoreProfileData.then(function (storeProfile) {
             $scope.storeProfiles = storeProfile.data;
         }, function () {
-            alert('Error in getting book records');
+            alert('Error in getting Store Profiles');
         });
     }
 });
