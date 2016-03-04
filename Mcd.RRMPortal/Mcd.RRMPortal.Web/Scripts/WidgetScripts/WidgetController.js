18c5d81003251b3d50c06d5bbc58f0c87cc5388f
ashenfern <ashenfern@gmail.com>
2016-03-04 11:08:49 +0530

    Checking changes + Commting new changes

---
+++ Mcd.RRMPortal/Mcd.RRMPortal.Web/Scripts/WidgetScripts/WidgetController.js
@@ -0,0 +1,14 @@
+﻿app.controller("widgetCtrl", function ($scope, widgetService) {
+    $scope.divBook = false;
+    GetAllBooks();
+    //To Get all book records  
+    function GetAllBooks() {
+        debugger;
+        var getBookData = widgetService.getBooks();
+        getBookData.then(function (book) {
+            $scope.books = book.data;
+        }, function () {
+            alert('Error in getting book records');
+        });
+    }
+});
